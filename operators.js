43de3cc090dbda04dd9db082022480c656d66e5a
Student <abishekkumara24csa@vetias.ac.in>
2026-06-12 11:18:22 +0530

    operators program

--- operators.js
+++ operators.js
@@ -1,9 +1,23 @@
 //Arthemitc operators
-let a = 10;
+/*let a = 10;
 let b = 5;
 console.log(a + b); // Addition
 console.log(a - b); // Subtraction
 console.log(a * b); // Multiplication
 console.log(a / b); // Division
 console.log(a % b); // Modulus
-console.log(a ** b); // Exponentiation
+console.log(a ** b); // Exponentiation*/
+
+//Assignment operators
+let c = 20;
+console.log(c); // 20
+c -= 5;
+console.log(c); // 15
+c *= 2;
+console.log(c); // 30
+c /= 3;
+console.log(c); // 10
+c %= 4;
+console.log(c); // 2
+c **= 3;
+console.log(c); // 8
