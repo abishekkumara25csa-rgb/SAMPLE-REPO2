019f01a7c1dfd89ab878fce9c3342e190428cb5d
Student <abishekkumara24csa@vetias.ac.in>
2026-06-12 10:20:07 +0530

    variables program

---
+++ variables.js
@@ -0,0 +1,13 @@
+//variables
+var name = "John";
+console.log(name);
+//let and const
+{
+let city = "New York";
+console.log(city);
+}
+//const cannot be reassigned
+const country = "USA"
+;
+console.log(country);
+//data types
