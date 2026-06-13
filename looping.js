1d87757027b73a1e91e46ce412431c7c7ffed202
Student <abishekkumara24csa@vetias.ac.in>
2026-06-13 13:45:08 +0530

    operators program

---
+++ looping.js
@@ -0,0 +1,27 @@
+console.log("for") 
+    for (let i = 1; i <=
+         10; i++) {
+        console.log(i+ " x 2 = " + i * 2);
+    }
+
+console.log("loop")
+    let students = ["Alice", "Bob", "Charlie"];
+    for (let i = 0; i < students.length; i++) {
+        console.log(students[i]);
+    }
+
+    console.log("while")
+    let count = 1;
+    while (count <= 10) {
+        console.log(count);
+        count++;
+    }
+
+    console.log("do while")
+    let num = 1;
+    do {
+        console.log(num);
+        num++;
+    } while (num <= 10);
+
+ 
